82513657a9bbb790c33ea7273109ec03a936a605
Arsh Ilahi <156427743+Arsh249@users.noreply.github.com>
2024-09-16 18:39:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+const openModalButton = document.getElementById('openModal');
+        const modal = document.getElementById('myModal');
+        const closeModalButton = document.querySelector('.close-modal');   
+
+
+        openModalButton.addEventListener('click', () => {
+            modal.style.display = 'block';
+        });
+
+        closeModalButton.addEventListener('click', () => {
+            modal.style.display   
+ = 'none';
+        });
+
+        window.addEventListener('click', (event) => {
+            if (event.target === modal) {
+                modal.style.display = 'none';
+            }
+        });
